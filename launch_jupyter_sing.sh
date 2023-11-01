fc6387f303f4ede63fc6edefeedf743576f6b7a0
KidElectric <brian.isett@gmail.com>
2023-11-01 10:31:31 -0400

    Working cell density extraction.

--- launch_jupyter_sing.sh
+++ launch_jupyter_sing.sh
@@ -5,6 +5,6 @@ PORT=$1
 PORT="${PORT:-8888}" #Switches port to default value if left empty
 PRJ="HCC-CBS-173-Hillman-BFerris-NRG-HN003-Vectra"
 singularity exec -B /ix/rbao/Projects/$PRJ:/mnt \
-    /ix/rbao/images/pathml_jupyter2.sif \
+    /ix/rbao/images/pathml_jupyter6_nv.sif \
     /opt/conda/envs/py38/bin/jupyter lab \
     --no-browser --port=$PORT --ip=0.0.0.0 
